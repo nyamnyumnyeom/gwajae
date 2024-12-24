5a91eccdf2bea7dbd133a7d007f56d3f67ff1d9d
nyamnyumnyeom <jincar000@naver.com>
2024-12-24 17:35:39 +0900

    필수1번 교체
    문제에 함수 밖에 둬야 하는거 못봄

--- 1번 문제/필수.cpp
+++ 1번 문제/필수.cpp
@@ -1,19 +1,43 @@
 ﻿#include <iostream>
-
 using namespace std;
 
-int avg = 0;
-int hap = 0;
-int arr[5];
+
+int suming(int arr[]) {
+    int i = 0;
+    int sum = 0;
+    for (i; i < 5; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+
+double avging(int* haping) {
+    double avvg = 0.0;
+    avvg = *haping / 5;
+    return avvg;
+}
 
 int main() {
-	int i = 0;
-	for (i;i < 5;i++) {
-		cin >> arr[i];
-		hap += arr[i];
-		avg = hap / 5;
-	}
-	cout << "합 : " << hap << endl;
-	cout << "평균 : " << avg;
-	return 0;
+    int arr[5];
+    int hap = 0;
+    int* haping = &hap;
+    double avg = 0.0;
+
+    cout << "숫자 5개를 입력해주세요 : " << endl;
+
+
+    for (int i = 0; i < 5;i++) {
+        cin >> arr[i];
+    }
+
+
+    hap = suming(arr);
+    avg = avging(haping);
+
+
+    cout << "합 : " << hap << endl;
+    cout << "평균 : " << avg << endl;
+
+    return 0;
 }
