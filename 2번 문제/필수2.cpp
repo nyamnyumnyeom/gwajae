b047e80ac17c4e57f3df06927c31d4545ac625ae
nyamnyumnyeom <jincar000@naver.com>
2024-12-24 20:01:04 +0900

    2번 필수 추가

---
+++ 2번 문제/필수2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+    virtual void makeSound() const = 0;
+    virtual ~Animal() {}
+};
+
+
+class Dog : public Animal {
+public:
+    void makeSound() const override {
+        cout << "월우렁뤙루어뤄우럴월월월!!!" << std::endl;
+    }
+};
+
+
+class Cat : public Animal {
+public:
+    void makeSound() const override {
+        cout << "냥냥" << std::endl;
+    }
+};
+
+class Cow : public Animal {
+public:
+    void makeSound() const override {
+        cout << "음뭐~~" << std::endl;
+    }
+};
+
+int main() {
+    const int m = 3;
+    Animal* animals[m] = { new Dog(), new Cat(), new Cow() };
+
+
+    for (int i = 0; i < m; i++) {
+        animals[i]->makeSound();
+    }
+
+
+    for (int i = 0; i < m; i++) {
+        delete animals[i];
+    }
+
+    return 0;
+}
